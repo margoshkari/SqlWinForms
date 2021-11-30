f578364755c01982a16eb2d729c01fde12317f04
Margo <student@24-04>
2021-11-30 14:59:41 +0200

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serverTB = new System.Windows.Forms.TextBox();
+            this.loginTB = new System.Windows.Forms.TextBox();
+            this.passwordTB = new System.Windows.Forms.TextBox();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // serverTB
+            // 
+            this.serverTB.Location = new System.Drawing.Point(12, 12);
+            this.serverTB.Name = "serverTB";
+            this.serverTB.Size = new System.Drawing.Size(73, 23);
+            this.serverTB.TabIndex = 0;
+            this.serverTB.Text = "Server";
+            this.serverTB.Click += new System.EventHandler(this.serverTB_Click);
+            // 
+            // loginTB
+            // 
+            this.loginTB.Location = new System.Drawing.Point(91, 12);
+            this.loginTB.Name = "loginTB";
+            this.loginTB.Size = new System.Drawing.Size(73, 23);
+            this.loginTB.TabIndex = 0;
+            this.loginTB.Text = "Login";
+            this.loginTB.Click += new System.EventHandler(this.loginTB_Click);
+            // 
+            // passwordTB
+            // 
+            this.passwordTB.Location = new System.Drawing.Point(170, 12);
+            this.passwordTB.Name = "passwordTB";
+            this.passwordTB.Size = new System.Drawing.Size(73, 23);
+            this.passwordTB.TabIndex = 0;
+            this.passwordTB.Text = "Password";
+            this.passwordTB.Click += new System.EventHandler(this.passwordTB_Click);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(254, 12);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(75, 23);
+            this.nextBtn.TabIndex = 1;
+            this.nextBtn.Text = "->";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 41);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(152, 397);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 450);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.passwordTB);
+            this.Controls.Add(this.loginTB);
+            this.Controls.Add(this.serverTB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox serverTB;
+        private System.Windows.Forms.TextBox loginTB;
+        private System.Windows.Forms.TextBox passwordTB;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection($@"Data Source={this.serverTB.Text};Initial Catalog={this.loginTB.Text};UID=student;Password={this.passwordTB.Text}"))
+                {
+                    connection.Open();
+                    MessageBox.Show("Connected");
+                    using (SqlCommand command = new SqlCommand(@"SELECT name FROM sys.databases;", connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                object name = reader.GetValue(0);
+                                this.treeView1.Nodes.Add(name.ToString());
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error");
+            }
+        }
+        private List<string> Select(string dbname)
+        {
+            List<string> list = new List<string>();
+            using (SqlConnection connection = new SqlConnection($@"Data Source={this.serverTB.Text};Initial Catalog={this.loginTB.Text};UID=student;Password={this.passwordTB.Text}"))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand($@"SELECT name FROM {dbname}.sys.tables;", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            object name = reader.GetValue(0);
+                            list.Add(name.ToString());
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+        private void serverTB_Click(object sender, EventArgs e)
+        {
+            this.serverTB.Text = string.Empty;
+        }
+
+        private void loginTB_Click(object sender, EventArgs e)
+        {
+            this.loginTB.Text = string.Empty;
+        }
+
+        private void passwordTB_Click(object sender, EventArgs e)
+        {
+            this.passwordTB.Text = string.Empty;
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            foreach (var item in Select((sender as TreeView).SelectedNode.Text))
+            {
+                (sender as TreeView).SelectedNode.Nodes.Add(item);
+            }
+        }
+    }
+}
